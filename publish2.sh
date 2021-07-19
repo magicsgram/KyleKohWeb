89575b38a61511368c7d2ce81f5260ca5684677d
Kyle Koh <kyle@zeusliving.com>
2021-07-19 11:11:14 -0700

    update publish

--- publish2.sh
+++ publish2.sh
@@ -1,2 +1,3 @@
 dotnet publish -c Release -r linux-x64 --self-contained=true -p:IncludeAllContentForSelfExtract=true -p:PublishTrimmed=true -o ./webpublish
 tar -zcvf conn.tar.gz webpublish/
+rm -rf webpublish/
