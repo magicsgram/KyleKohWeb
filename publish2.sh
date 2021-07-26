6d89cdbb33ac928a5af1673ab8a4acee63e56892
Kyle Koh <kyle@zeusliving.com>
2021-07-26 10:42:10 -0700

    remove old file

--- publish2.sh
+++ publish2.sh
@@ -1,3 +1,4 @@
+rm conn.tar.gz
 dotnet publish -c Release -r linux-x64 --self-contained=true -p:IncludeAllContentForSelfExtract=true -p:PublishTrimmed=true -o ./webpublish
 tar -zcvf conn.tar.gz webpublish/
 rm -rf webpublish/
