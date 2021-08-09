8dbca72f615d286ce1ccebf5a71ee49f26a75967
Kyle <magicsgram@outlook.com>
2021-08-09 00:01:45 -0700

    update compression

--- publish2.bat
+++ publish2.bat
@@ -1,4 +1,4 @@
-wsl rm tempconn.tar.gz
+wsl rm tempconn.tar.xz
 dotnet publish -c Release -r linux-x64 --self-contained=true -p:IncludeAllContentForSelfExtract=true -p:PublishTrimmed=true -o ./webpublish
-wsl tar -zcvf tempconn.tar.gz webpublish/
+wsl XZ_OPT=-9 tar -Jcvf tempconn.tar.xz webpublish/
 wsl rm -rf webpublish/
--- publish2.sh
+++ publish2.sh
@@ -1,4 +1,4 @@
 rm tempconn.tar.gz
 dotnet publish -c Release -r linux-x64 --self-contained=true -p:IncludeAllContentForSelfExtract=true -p:PublishTrimmed=true -o ./webpublish
-tar -zcvf tempconn.tar.gz webpublish/
+XZ_OPT=-9 tar -Jcvf tempconn.tar.xz webpublish/
 rm -rf webpublish/
