86ca35f90996b5b2d6cea7b12cf9c050c17abee8
Kyle Koh <kyle@zeusliving.com>
2021-11-24 14:02:36 -0800

    fix

--- publish2.sh
+++ publish2.sh
@@ -1,4 +1,4 @@
-rm tempconn.tar.gz
+rm tempconn.tar.xz
 dotnet publish -c Release -r linux-x64 --self-contained=true -p:IncludeAllContentForSelfExtract=true -p:PublishTrimmed=true -o ./webpublish
 XZ_OPT=-9 tar -Jcvf tempconn.tar.xz webpublish/
 rm -rf webpublish/
