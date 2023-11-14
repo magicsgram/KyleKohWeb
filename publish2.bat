736e80df7ead79b553103eec1b1de86e10b5a479
Kyle Koh <magicsgram@outlook.com>
2023-11-14 06:20:59 -0800

    raise priority level for builder script

--- publish2.bat
+++ publish2.bat
@@ -1,4 +1,4 @@
 wsl rm tempconn.tar.xz
-start /B /low /wait dotnet publish -c Release -r linux-x64 --self-contained=true -p:IncludeAllContentForSelfExtract=true -p:PublishTrimmed=true -o ./webpublish
+dotnet publish -c Release -r linux-x64 --self-contained=true -p:IncludeAllContentForSelfExtract=true -p:PublishTrimmed=true -o ./webpublish
 wsl XZ_OPT=-9 tar -Jcvf tempconn.tar.xz webpublish/
 wsl rm -rf webpublish/
